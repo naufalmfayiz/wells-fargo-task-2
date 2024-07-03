212ad3b363f326b36629ad2e087aca52dcd14fdd
naufalmfayiz <fayiznaufal@gmail.com>
2024-07-03 20:25:26 +0700

    Implement Data

---
+++ src/main/java/com/wellsfargo/counselor/entity/Client.java
@@ -0,0 +1,12 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import java.util.List;
+
+@Entity
+public class Client {
+  
+}
